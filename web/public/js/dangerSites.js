bca041325c37a2066f0c796340c6217d8b572982
Eric Pollmann <eric@pollmann.net>
2016-09-16 14:36:09 -0700

    Compute proper geoFire query radius on each bounds change.  Code cleanups, remove unused html5 geolocation.

--- web/public/js/dangerSites.js
+++ web/public/js/dangerSites.js
@@ -1,88 +1,72 @@
-// Global map variable
+var defaults = {
+  radiusInKm: 10,  // Query radius for danger sites
+  zoomLevel: 14,
+  center: [37.3842209, -122.1142677]
+}
 var map;
-
-// Query radius
-var radiusInKm = 10;
-
-// Get a reference to the Firebase public transit open data set
-// var transitFirebaseRef = new Firebase("https://publicdata-transit.firebaseio.com/")
-var dangerFirebaseRef = new Firebase("https://superfund-8d935.firebaseio.com/")
-
-// Create a new GeoFire instance, pulling data from the public danger data
-var geoFire = new GeoFire(dangerFirebaseRef.child("_geofire"));
-
-// Query params parsed from url (see function at end)
 var urlParams;
+
+// Public danger database
+var dangerFirebaseRef = new Firebase("https://superfund-8d935.firebaseio.com/")
+var geoFire = new GeoFire(dangerFirebaseRef.child("_geofire"));
 
 /*************/
 /*  GEOQUERY */
 /*************/
-// Keep track of all of the sites currently within the query
-var sitesInQuery = {};
+var sitesInQuery = {};  // All sites currently onscreen
 
 var geoQuery = null;
 var startQuery = function(e) {
   var lat = parseFloat(urlParams.latitude);
-  if (isNaN(lat)) {
-    lat = 37.3842209;
-  }
   var lon = parseFloat(urlParams.longitude);
-  if (isNaN(lon)) {
-    lon = -122.1142677;
+  if (isNaN(lat)) {
+    lat = defaults.center[0];
   }
-  
-  defaultCenter = [lat, lon];
-  initializeMap(defaultCenter);
+  if (isNaN(lon)) {
+    lon = defaults.center[1];
+  }
+  center = [lat, lon];
+  initializeMap(center);
 
   // Create a new GeoQuery instance
   geoQuery = geoFire.query({
-    center: defaultCenter,
-    radius: radiusInKm
+    center: center,
+    radius: defaults.radiusInKm
   });
 
-  /* Adds new site markers to the map when they enter the query */
+  // Add new site markers to the map when they enter the query
   geoQuery.on("key_entered", function(siteId, siteLocation) {
-    // Specify that the site has entered this query
     sitesInQuery[siteId] = true;
 
     // Look up the site's data
     dangerFirebaseRef.child("sites").child(siteId).once("value", function(dataSnapshot) {
-      // Get the site data from the Open Data Set
       site = dataSnapshot.val();
 
       // If the site has not already exited this query in the time it took to look up its data, add it to the map
       if (site !== null && sitesInQuery[siteId] === true) {
-        // Add the site to the list of sites in the query
         sitesInQuery[siteId] = site;
-
-        // Create a new marker for the site
         site.marker = createSiteMarker(site, getSiteColor(site));
       }
     });
   });
 
-  /* Moves sites markers on the map when their location within the query changes */
+  // Move sites markers on the map when their location within the query changes
   geoQuery.on("key_moved", function(siteId, siteLocation) {
-    // Get the site from the list of sites in the query
     var site = sitesInQuery[siteId];
-
-    // Animate the site's marker
     if (typeof site !== "undefined" && typeof site.marker !== "undefined") {
       site.marker.animatedMoveTo(siteLocation);
     }
   });
 
-  /* Removes site markers from the map when they exit the query */
+  // Remove site markers from the map when they exit the query */
   geoQuery.on("key_exited", function(siteId, siteLocation) {
-    // Get the site from the list of sites in the query
     var site = sitesInQuery[siteId];
 
-    // If the site's data has already been loaded from the Open Data Set, remove its marker from the map
+    // If the site's data has already been loaded from the database, remove its marker from the map
     if (site !== true) {
       site.marker.setMap(null);
     }
 
-    // Remove the site from the list of sites in the query
     delete sitesInQuery[siteId];
   });
 }
@@ -90,33 +74,36 @@ var startQuery = function(e) {
 /*****************/
 /*  GOOGLE MAPS  */
 /*****************/
-/* Initializes Google Maps */
+
 function initializeMap(center) {
   // Get the location as a Google Maps latitude-longitude object
   var loc = new google.maps.LatLng(center[0], center[1]);
 
-  // Create the Google Map
+  // Create the map
   map = new google.maps.Map(document.getElementById("map-canvas"), {
     center: loc,
-    zoom: 14,
+    zoom: defaults.zoomLevel,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
-  //Update the query's criteria every time the circle is dragged
+  // Update the query's criteria every time the map is dragged
   var updateCriteria = _.debounce(function() {
-    var latLng = map.getCenter();
+    var center = map.getCenter();
+    var mapBoundNorthEast = map.getBounds().getNorthEast();
+    var radiusKm = google.maps.geometry.spherical.computeDistanceBetween(mapBoundNorthEast, center)/1000;
     geoQuery.updateCriteria({
-      center: [latLng.lat(), latLng.lng()],
-      radius: radiusInKm
+      center: [center.lat(), center.lng()],
+      radius: radiusKm
     });
   }, 10);
-  map.addListener("center_changed", updateCriteria);
+  map.addListener("bounds_changed", updateCriteria);
 }
 
 /**********************/
 /*  HELPER FUNCTIONS  */
 /**********************/
-/* Adds a marker for the site to the map */
+
+// Add a marker for the site to the map */
 function createSiteMarker(site, siteColor) {
   var marker = new google.maps.Marker({
     icon: "https://chart.googleapis.com/chart?chst=d_bubble_icon_text_small&chld=caution|edge_bc|" + site.name + "|" + siteColor + "|eee",
@@ -128,17 +115,17 @@ function createSiteMarker(site, siteColor) {
   return marker;
 }
 
-/* Returns a blue color code for outbound sites or a red color code for inbound sites */
+// Return appropriate color based on site
 function getSiteColor(site) {
-  return ((site.dirTag && site.dirTag.indexOf("OB") > -1) ? "50B1FF" : "FF6450");
+  return 'FF6450';
 }
 
-/* Returns true if the two given coordinates are approximately equivalent */
+// Return true if the two given coordinates are approximately equivalent
 function coordinatesAreEquivalent(coord1, coord2) {
   return (Math.abs(coord1 - coord2) < 0.000001);
 }
 
-/* Animates the Marker class (based on https://stackoverflow.com/a/10906464) */
+// Animate the Marker class (based on https://stackoverflow.com/a/10906464)
 google.maps.Marker.prototype.animatedMoveTo = function(newLocation) {
   var toLat = newLocation[0];
   var toLng = newLocation[1];
@@ -159,11 +146,11 @@ google.maps.Marker.prototype.animatedMoveTo = function(newLocation) {
       if (percent >= 1) {
         window.clearInterval(interval);
       }
-    }.bind(this), 50);
+    }.bind(this), 15);
   }
 };
 
-// Query param parsing
+// Parse query params into urlParams global
 (window.onpopstate = function () {
     var match,
         pl     = /\+/g,  // Regex for replacing addition symbol with a space
