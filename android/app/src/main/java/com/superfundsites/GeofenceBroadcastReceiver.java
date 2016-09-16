54e7ae5d16c3924ff340f0a6c4fb72facfd38f30
Alec Go <go@uber.com>
2016-09-16 10:19:26 -0700

    Initial check-in for Android code

---
+++ android/app/src/main/java/com/superfundsites/GeofenceBroadcastReceiver.java
@@ -0,0 +1,49 @@
+package com.superfundsites;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+public class GeofenceBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        GeofencingEvent event = GeofencingEvent.fromIntent(intent);
+        String transition = mapTransition(event.getGeofenceTransition());
+
+        Intent notificationIntent = new Intent(context, DetailsActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+                notificationIntent, 0);
+
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Geofence action")
+                .setContentIntent(pendingIntent)
+                .setContentText(transition)
+                .setTicker("Geofence action")
+                .addAction(R.mipmap.ic_launcher, "View details", pendingIntent)
+                .build();
+
+        nm.notify(0, notification);
+    }
+
+    private String mapTransition(int event) {
+        switch (event) {
+            case Geofence.GEOFENCE_TRANSITION_ENTER:
+                return "ENTER";
+            case Geofence.GEOFENCE_TRANSITION_EXIT:
+                return "EXIT";
+            default:
+                return "UNKNOWN";
+        }
+    }
+}
